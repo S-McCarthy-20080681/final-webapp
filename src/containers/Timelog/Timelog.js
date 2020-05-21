d1ae28b1fbb3d36ec5adfc0df540541ad88e4f4d
Susan McCarthy <57634675+S-McCarthy-20080681@users.noreply.github.com>
2020-05-21 19:03:31 +0100

    App skeleton and test time log list

---
+++ src/containers/Timelog/Timelog.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Timelog extends Component {
+    render () {
+        return (
+            <div>
+                <div>April</div>
+                <div>May</div>
+                <div>June</div>
+            </div>
+        );
+    }
+}
+
+export default Timelog;
