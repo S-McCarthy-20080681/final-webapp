bf84013eeeeb58989d5ad32760f8686761843bb2
Susan McCarthy <57634675+S-McCarthy-20080681@users.noreply.github.com>
2020-06-05 19:42:48 +0100

    Made some CSS/layout changes, added a background, and tidied up duplicate code between App.js and Layout.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
-import Layout from './components/layout/Layout';
+import Layout from './components/Layout';
 import Timelogs from './containers/Timelog/Timelogs';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import EntryForm from './components/form/EntryForm';
+//import EntryForm from './components/form/EntryForm';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="App-header">Online Journal</h1>
+    
     <div className="timelogs">
       <Layout>
         <Timelogs />
       </Layout>
-    </div>
     </div>
   );
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../App.css';
+import EntryForm from './form/EntryForm';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+
+const layout = (props) => (
+    <div className="layout">
+    <div className="container-fluid">
+
+        <div classname="row">
+            <div className="col-md-6 ">
+                <h1 className="App-header">Online Journal</h1>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-md-4">
+                <div className="form">
+                    <EntryForm />
+                </div>
+            </div>
+
+            <div className="col-md-10">
+                <div className="toolbar">
+                    <h3>Choose A Month</h3>
+                        <p>Clicking on a month displays the entries for that month:</p>
+                </div>
+            </div>
+        
+
+            <main className="col-md-10">
+                {props.children}
+            </main>
+        </div> 
+    </div>
+    </div>
+);
+
+export default layout;
